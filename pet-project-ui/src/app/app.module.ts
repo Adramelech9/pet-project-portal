a403d0db164ff9cec4528d40426fcf681399f541
Denys Lapitan <d.lapitan@itransition.com>
2021-12-29 16:35:26 +0200

    initial commit

--- pet-project-ui/src/app/app.module.ts
+++ pet-project-ui/src/app/app.module.ts
@@ -13,6 +13,9 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+
+  ]
 })
 export class AppModule { }
