e8967a0a9efbcb1d6c418069380ec0ae8466d4ce
Denis Lapitan <d.lapitan@itransition.com>
2021-12-30 18:02:04 +0200

    add correct links

--- pet-project-ui/src/app/app-routing.module.ts
+++ pet-project-ui/src/app/app-routing.module.ts
@@ -3,11 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent} from './client/client.component';
 import { InsuranceContractComponent} from './insurance-contract/insurance-contract.component';
 import { AddClientComponent } from './add-client/add-client.component';
+import { ClientInfoComponent } from './client-info/client-info.component';
 
 const routes: Routes = [
   { path: 'insurance-contract-component', component: InsuranceContractComponent},
-  { path: 'client-component', component: ClientComponent},
-  { path: 'add-client-component', component: AddClientComponent}
+  {
+    path: 'client-component',
+    component: ClientComponent,
+    children: [
+      { path: 'add-client-component', component: AddClientComponent},
+      { path: 'client-info-component', component: ClientInfoComponent}
+    ]
+  }
 ];
 
 @NgModule({
--- pet-project-ui/src/app/app.module.ts
+++ pet-project-ui/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ClientComponent } from './client/client.component';
 import { InsuranceContractComponent } from './insurance-contract/insurance-contract.component';
 import { AddClientComponent } from './add-client/add-client.component';
+import { ClientInfoComponent } from './client-info/client-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientComponent,
     InsuranceContractComponent,
-    AddClientComponent
+    AddClientComponent,
+    ClientInfoComponent
   ],
   imports: [
     BrowserModule,
